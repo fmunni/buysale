83037fcf04b78415c393187a5270b6391a20204e
Farhana Munni <minniekader@gmail.com>
2019-07-30 21:58:59 -0500

    added ui

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
 const express = require("express");
+const path = require("path");
+
 const app = express();
 const port = 8080;
+
+const bodyParser = require("body-parser");
+app.use(bodyParser.json());
+
+app.use(express.static(path.join(__dirname, "ui/build")));
+
+app.get("/getuser", (req, res) =>
+  res.send({ users: ["mak", "munni", "farhana"] })
+);
+
+app.post("/finduser", (req, res) => {
+  //console.log("received request ", req.body.name);
+  res.send(`<h1> my name is ${req.body.name} </h1>`);
+});
 
 app.get("/", (req, res) => res.send("HomePage for my ecommerce site."));
 
---
+++ ui/src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <p>This website is for buying and selling your books.</p>
+        <a className="App-link" href="https://cogninvest.com" target="_blank">
+          Congninvest.com
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
